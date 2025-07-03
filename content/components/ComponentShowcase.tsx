5b4dd8b298da853eff41e2828944954fd4c51c7f
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-07-03 17:02:01 +0530

    fix: fixed theme issue in the ComponentShowcase component. and added a refresh button to fecilitate components like the text animation component.

--- content/components/ComponentShowcase.tsx
+++ content/components/ComponentShowcase.tsx
@@ -1,36 +1,52 @@
-'use client'
+"use client"
 
-import React, { useState } from 'react'
-import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock'
+import React, { useState } from "react"
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock"
+import { IconRotate } from "@tabler/icons-react";
+import { Button } from "@/components/ui/Btn";
 
-export const ComponentShowcase = ({ component, code }: { component: React.ReactNode, code: string }) => {
+export const ComponentShowcase = ({ component, code, showRefresh }: { component: React.ReactNode, code: string, showRefresh?: boolean } = { showRefresh: false, component: <></>, code: "" }) => {
   const [isCode, setIsCode] = useState(false);
+  const [refreshKey, setRefreshKey] = useState(0);
 
   return (
-    <div className='flex flex-col gap-4'>
-      <div className='flex gap-4'>
-        <div
-          className={`cursor-pointer border-b transition-colors ${isCode ? 'text-muted-foreground' : 'text-white border-b-white'}`}
-          onClick={() => setIsCode(false)}
-        >
-          Preview
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4">
+          <div
+            className={`cursor-pointer border-b transition-colors ${isCode ? "text-muted-foreground" : "text-primary border-b-primary"}`}
+            onClick={() => setIsCode(false)}
+          >
+            Preview
+          </div>
+          <div
+            className={`cursor-pointer border-b transition-colors ${isCode ? "text-primary border-b-primary" : "text-muted-foreground"}`}
+            onClick={() => setIsCode(true)}
+          >
+            Code
+          </div>
         </div>
-        <div
-          className={`cursor-pointer border-b transition-colors ${isCode ? 'text-white border-b-white' : 'text-muted-foreground'}`}
-          onClick={() => setIsCode(true)}
-        >
-          Code
-        </div>
+        {
+          showRefresh && !isCode && (
+            <Button
+              size={"icon"}
+              variant={"secondary"}
+              onClick={() => setRefreshKey(k => k + 1)}
+            >
+              <IconRotate size={18} />
+            </Button>
+          )
+        }
       </div>
 
       {isCode ? (
-        <CodeBlock lang="tsx" keepBackground className='max-h-[25rem] overflow-auto'>
+        <CodeBlock lang="tsx" keepBackground className="max-h-[25rem] overflow-auto">
           <Pre className="px-6 font-mono">
             {code}
           </Pre>
         </CodeBlock>
       ) : (
-        <div className='h-[25rem] w-full rounded-xl border border-border p-4 flex flex-col justify-center items-center'>
+        <div key={refreshKey} className="h-[25rem] w-full rounded-xl border border-border p-4 flex flex-col justify-center items-center">
           {component}
         </div>
       )}
